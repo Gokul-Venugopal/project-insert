d26461bb111e7054faee12dd1adb147ca6ba4f17
Gokul-Venugopal <88612532+Gokul-Venugopal@users.noreply.github.com>
2021-08-25 19:23:39 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,75 @@
+
+<!DOCTYPE html>
+<html>
+ <div class="container" >
+	<head > 
+		<title>Appointment </title>
+		<link rel="stylesheet" href="bootstrap.min.css">
+    </head>
+	<h1 style="color: darkred;">Online Appointment Booking</h1>	
+	<br>
+    <marquee behavior="scroll" direction="right">Open from 9am To 6pm Monday To Friday</marquee>
+
+
+	<a href="http://localhost/project/index.php">
+    <button>Home</button>
+    </a>
+    
+	<a href="http://localhost/project/retrieve.php">
+    <button>Retrieve</button>
+    </a>
+
+	<a href="http://localhost/project/update.php">
+    <button>Update</button>
+    </a>
+	<a href="http://localhost/project/delete.php">
+    <button>Delete</button>
+    </a>
+
+   <br><br>
+  <body >
+     <style>
+   body {
+            background-image: url('https://as1.ftcdn.net/v2/jpg/04/46/17/50/1000_F_446175004_65bFJcZd4UDzWWIda6Fz2maMrZkOO4NL.jpg');
+			position: center;
+        }
+     </style>
+	<form method="post" action="process.php" >
+	 
+		<b>First name:</b> <br>
+		<input type="text" name="firstname" required>
+		<br><br>
+		<b>Last name:</b><br>
+		<input type="text" name="lastname" required>
+		<br><br>
+		<b>Age:</b><br>
+		<input type="text" name="Age" required>
+		<br><br>
+		<b>Adhaar Number:</b><br>
+		<input type="number" name="Adhaar_Number"  required>
+		<br><br>
+		<b>Previous Health Condition:</b><br>
+		<input type="text" name="Previous_Health_Condition" required>
+		<br><br>
+		<b>Present Health Condition:</b><br>
+		<input type="text" name="Present_Health_Condition" required>
+		<br><br>
+		<b>Phonenumber</b><br>
+		<input type="number" name="phonenumber" required>
+		<br><br>
+		<b>Doctor Specialization:</b><br>
+		<input type="text" name="specs" required>
+		<br><br>
+
+		<b>Date of Appointment:</b><br>
+		<input type="Date" name="Date_of_Appointment" required>
+		<br><br>
+		<b>Time Slot:</b><br>
+		<input type="time" name="Time_Slot" required>
+		<br><br>
+
+		<input type="submit" name="save" value="Book Appointment">
+	</div>
+	</form>
+  </body>
+</html>
---
+++ process.php
@@ -0,0 +1,52 @@
+
+<h1 style="color: darkred;">Online Appointment Booking</h1>
+ <h2 style="color: darkred;">Record inserted successfully</h2>
+<br><br>
+
+<a href="http://localhost/project/index.php">
+    <button>Home</button>
+    </a>
+    
+	<a href="http://localhost/project/retrieve.php">
+    <button>Retrieve</button>
+    </a>
+
+	<a href="http://localhost/project/update.php">
+    <button>Update</button>
+    </a>
+	<a href="http://localhost/project/delete.php">
+    <button>Delete</button>
+</a>
+
+<style>
+   body {
+            background-image: url('https://www.maketecheasier.com/assets/uploads/2016/09/settings-app-not-working-featured.jpg');
+			position: center;
+        }
+     </style>
+
+<?php
+include_once 'database.php';
+if(isset($_POST['save']))
+{	 
+	 $firstname = $_POST['firstname'];
+	 $lastname = $_POST['lastname'];
+	 $Age = $_POST['Age'];
+	 $Adhaar_Number = $_POST['Adhaar_Number'];
+	 $Previous_Health_Condition = $_POST['Previous_Health_Condition'];
+	 $Present_Health_Condition = $_POST['Present_Health_Condition'];
+	 $phonenumber = $_POST['phonenumber'];
+	 $specs = $_POST['specs'];
+	 $Date_of_Appointment = $_POST['Date_of_Appointment'];
+	 $Time_Slot = $_POST['Time_Slot'];
+	 $sql = "INSERT INTO record (firstname,lastname,Age,Adhaar_Number,Previous_Health_Condition,Present_Health_Condition,phonenumber,specs,Date_of_Appointment,Time_Slot)
+	 VALUES ('$firstname','$lastname','$Age','$Adhaar_Number','$Previous_Health_Condition','$Present_Health_Condition','$phonenumber','$specs','$Date_of_Appointment','$Time_Slot')";
+	 if (mysqli_query($conn, $sql)) {
+		echo 'View All record inserted successfully !</a>';
+	 } else {
+		echo "Error: " . $sql . "
+" . mysqli_error($conn);
+	 }
+	 mysqli_close($conn);
+}
+?>
